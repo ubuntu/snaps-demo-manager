867294faa8b87724f0cb7387acadafb1f38778ac
Didier Roche <didrocks@ubuntu.com>
2016-10-26 10:50:27 +0200

    Add straight yaml loader for instructions

---
+++ manager/loader.go
@@ -0,0 +1,39 @@
+package manager
+
+import (
+	"fmt"
+	"io/ioutil"
+	"path"
+
+	"gopkg.in/yaml.v2"
+)
+
+type instructionsFormat struct {
+	Snaps map[string][]struct {
+		Origin string
+		Wait   int
+		Next   string
+	}
+}
+
+func init() {
+	f := path.Join("instructions.yaml")
+	var instructions instructionsFormat
+
+	// load instructions
+	d, err := ioutil.ReadFile(f)
+	if err != nil {
+		// no file available: can be just installed with no instructions
+		return
+	}
+	if err = yaml.Unmarshal(d, &instructions); err != nil {
+		panic("Couldn't deserialized instructions from file:" + err.Error())
+	}
+
+	fmt.Printf("%+v\n", instructions)
+
+}
+
+func F() {
+
+}
