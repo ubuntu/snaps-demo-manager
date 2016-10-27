dcf9c4a67822c32956cdeea8961104e47e783cac
Didier Roche <didrocks@ubuntu.com>
2016-10-27 10:01:51 +0200

    Base structure on snap tracking routine

--- manager/snaps.go
+++ manager/snaps.go
@@ -1,8 +1,10 @@
 package manager
 
 import (
+	"sync"
 	"time"
 
+	"github.com/ubuntu/snaps-manager/logger"
 	"github.com/ubuntu/snaps-manager/state"
 )
 
@@ -10,4 +12,41 @@ type snapState struct {
 	state.State
 	WaitTime time.Duration
 	Action   state.Transition
+}
+
+// Snap on the system.
+type Snap struct {
+	Name         string
+	currentState snapState
+	Instructions chan state.SnapInstructions
+	Stop         chan bool
+}
+
+// NewSnap initializes a new snap object.
+func NewSnap(name string) *Snap {
+	logger.Info("Prepare new snap %s", name)
+	s := &Snap{Name: name}
+	s.Instructions = make(chan state.SnapInstructions)
+	s.Stop = make(chan bool)
+	return s
+}
+
+// Track and manage snap lifecycle.
+func (s *Snap) Track(wg *sync.WaitGroup) {
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		defer logger.Info("Stop tracking %s", s.Name)
+
+		logger.Info("Tracking %s", s.Name)
+		for {
+			select {
+			case <-s.Instructions:
+				logger.Info("New set of instructions received for %s", s.Name)
+
+			case <-s.Stop:
+				return
+			}
+		}
+	}()
 }
