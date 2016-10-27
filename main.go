5fc6954547e44b69fb001194efa8cd7b1537a6d5
Didier Roche <didrocks@ubuntu.com>
2016-10-27 13:49:06 +0200

    Enable starting and stopping tracking snaps from config
    Send as well corresponding instructions

--- main.go
+++ main.go
@@ -2,12 +2,26 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 
 	"github.com/ubuntu/snaps-manager/client"
+	"github.com/ubuntu/snaps-manager/manager"
+	"github.com/ubuntu/snaps-manager/state"
 )
 
+var currentSnaps map[string]*manager.Snap
+
 func main() {
+
+	wg := sync.WaitGroup{}
+	currentSnaps = make(map[string]*manager.Snap)
+
+	for name, p := range state.AllSnapsProperty {
+		startAndStopTracking(name, p, &wg)
+	}
+	wg.Done()
+	return
 	c := client.C
 	snap, r, err := c.Snap("consul")
 	fmt.Printf("%+v\n", snap)
@@ -27,5 +41,18 @@ func main() {
 		fmt.Println(change.Status)
 		fmt.Println(change.Ready)
 		time.Sleep(time.Duration(time.Second))
+	}
+}
+
+func startAndStopTracking(name string, p state.SnapProperty, wg *sync.WaitGroup) {
+	s, trackingExists := currentSnaps[name]
+	if p.Enabled && !trackingExists {
+		s := manager.NewSnap(name)
+		s.Track(wg)
+		s.Instructions <- p.Instructions
+		currentSnaps[name] = s
+	} else if !p.Enabled && trackingExists {
+		s.Stop <- true
+		delete(currentSnaps, name)
 	}
 }
